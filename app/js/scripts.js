f8ebec8eafd11fed517376584789a3d0e839f046
Petr Petukhov <kotov0991@gmail.com>
2018-06-07 02:08:49 +0300

    tariffs

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -91,6 +91,8 @@ $(function() {
 		}
 	});
 
+
+	// Jquery form validation
 	$(".login__form form").validate({
 		rules: {
 			email: {
@@ -107,12 +109,133 @@ $(function() {
 			password: "Пожалуйста введите пароль",
 		}
 	});
-
 	$('.login__form input').change(function() {
 		if($(".login__form form").valid()) {
 			$('.mfp-bg, .login').addClass('formValid');
 		}
 	})
+
+
+	// Scroll Animation
+	$(window).scroll(function(){
+		$('.animate').each(function(i, el){
+			if($(window).width() > 1199){
+				if ($(window).scrollTop() > $(el).offset().top - $(window).height() + 100) {
+					$(el).addClass('animated');
+				};
+			};
+		});
+	});
+
+
+
+
+	// tariffs page tabs
+	$(".purchase__content-block").not(":first").hide();
+
+	$(".purchase__tab").click(function() {
+		$(".purchase__tab").removeClass("purchase__tab--active").eq($(this).index()).addClass("purchase__tab--active");
+		$(".purchase__content-block").hide().eq($(this).index()).fadeIn("normal");
+	}).eq(0).addClass("purchase__tab--active");
+
+	// tariffs choose
+	$('.purchase__table-tariff').click(function() {
+		if( !$('.purchase__tables').hasClass('tariff-choosen') ) {
+			$('.purchase__table-tariff').removeClass('purchase__table-tariff--active');
+			$(this).addClass('purchase__table-tariff--active');
+		}
+		if( ($(window).width() < 1200 ) && (!$('.purchase__tables').hasClass('tariff-choosen')) ) {
+			$('.purchase__table-tariff').removeClass('purchase__table-tariff--active');
+			$(this).addClass('purchase__table-tariff--active');
+			$('body,html').animate({scrollTop: $('.purchase__order').offset().top}, 1000);
+		}
+	});
+
+	$('.purchase__order-back').click(function() {
+		$('.purchase__order').addClass('settings');
+		$('.purchase__tables').removeClass('tariff-choosen');
+	});
+
+	$('.purchase__btn-settings').click(function(e) {
+		if ( $('.purchase__table-tariff').hasClass('purchase__table-tariff--active') ) {
+			$('.purchase__order').removeClass('settings');
+			$('.purchase__tables').addClass('tariff-choosen');
+		}
+		e.preventDefault;
+	});
+
+
+	// tariffs page jquery form validation
+	$(".purchase__order form").validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			name: "required",
+			phone: "required",
+			inn: "required"
+		},
+		messages: {
+			email: {
+				required: "Пожалуйста введите email",
+				email: "Неккоректный email"
+			},
+			name: "Пожалуйста введите имя",
+			phone: "Пожалуйста введите контактный номер",
+			inn: "Пожалуйста введите ИНН организации"
+		}
+	});
+	$('.purchase__order-form input').change(function() {
+		if($(".purchase__order form").valid()) {
+			$('.purchase__order').addClass('formValid');
+		}
+	})
+
+
+
+
+
+
+
+	// Custom select
+	$(".my-select").each(function() {
+		var classes = $(this).attr("class"),
+				id      = $(this).attr("id"),
+				name    = $(this).attr("name");
+		var template =  '<div class="' + classes + '">';
+				template += '<span class="my-select-trigger">' + $(this).attr("placeholder") + '</span>';
+				template += '<div class="my-options">';
+				$(this).find("option").each(function() {
+					template += '<span class="my-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+				});
+		template += '</div></div>';
+		
+		$(this).wrap('<div class="my-select-wrapper"></div>');
+		$(this).after(template);
+	});
+	$(".my-option:first-of-type").hover(function() {
+		$(this).parents(".my-options").addClass("option-hover");
+	}, function() {
+		$(this).parents(".my-options").removeClass("option-hover");
+	});
+	$(".my-select-trigger").on("click", function() {
+		$(".my-select").removeClass("opened");
+		$('html').one('click',function() {
+			$(".my-select").removeClass("opened");
+		});
+		$(this).parents(".my-select").toggleClass("opened");
+		event.stopPropagation();
+	});
+	$(".my-option").on("click", function() {
+		$(this).parents(".my-select-wrapper").find("select").val($(this).data("value"));
+		$(this).parents(".my-options").find(".my-option").removeClass("selection");
+		$(this).addClass("selection");
+		$(this).parents(".my-select").removeClass("opened");
+		$(this).parents(".my-select").find(".my-select-trigger").text($(this).text());
+	});
+
+
 
 
 });
